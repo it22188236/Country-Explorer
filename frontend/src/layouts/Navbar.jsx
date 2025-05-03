d5301b6608e6b1d69b5331ae2f37c3363a27b1e9
it22188236 <144685319+it22188236@users.noreply.github.com>
2025-05-03 22:33:13 +0530

    Update index.html

--- frontend/src/layouts/Navbar.jsx
+++ frontend/src/layouts/Navbar.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import './Navbar.css';
+import '../layouts/Navbar.css';
 import { useNavigate } from 'react-router-dom';
 
 const Navbar = ({ showBackButton, onBack }) => {
